d1bb243344e14ae42fbb44a4554ebf2478d1980f
swapnilshetgar <shetgarswapnil@gmail.com>
2025-04-24 15:01:06 +0530

    Add files via upload
    Automate daily backups of specific directories (/home/sapi/project).
    
    Compress using tar.gz,  move to a backup drive /home/sapi/project/archive.

---
+++ archive_script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#$Revision:001$
+#variable declaration 
+
+BASE=/home/sapi/project
+DAYS=10
+DEPTH=1
+RUN=0
+
+
+#check dir present or not 
+
+if [ ! -d $BASE ]
+then     
+
+   echo "directory does not exist: $BASE"
+        exit 1
+fi
+
+#create archive folder if not present
+
+if [ ! -d $BASE/archive ]
+        then
+                mkdir $BASE/archive
+fi
+
+
+#find the list of files larger than 20MB
+
+for i in `find $BASE -maxdepth $DEPTH -type f -size +20M`
+do
+        if [ $RUN -eq 0  ]
+        then
+        gzip $i || exit 1
+        mv $i.gz  $BASE/archive || exit 3
+        echo "Archive file created successfully in $BASE/archive "      
+        fi
+done
